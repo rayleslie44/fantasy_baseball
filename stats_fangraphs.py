7bfb85e76abf6c4b53ea9a7f030b1dacc66316ce
rayleslie44 <rayleslie44@gmail.com>
2022-09-04 09:43:42 -0400

    updated stats_fangraphs.py to return column key .txt files

--- stats_fangraphs.py
+++ stats_fangraphs.py
@@ -4,6 +4,7 @@ import numpy as np
 from idmap import rosters_fangraphs
 from pybaseball import batting_stats
 from pybaseball import pitching_stats
+import sys
 
 # create function to get fangraphs data for batters and pitchers
 def get_fangraphs_data(x, y):
@@ -30,6 +31,20 @@ def get_fangraphs_data(x, y):
 
         # fill team id nulls with 0 to denote player is free agent (not rostered)
         stats['team_id'] = stats['team_id'].fillna(0).astype(int)
+
+        # save a reference to the original standard output
+        original_stdout = sys.stdout
+
+        with open('colkeys_fangraphs_batter.txt', 'w') as f:
+
+            # change the standard output to the file created
+            sys.stdout = f
+
+            # print column index and name to file
+            print(list(enumerate(stats.columns)))
+
+            # reset the standard output to its original output
+            sys.stdout = original_stdout
 
         # use certain metric columns only
         df = stats.iloc[:, np.r_[0:6, 7, 12:16, 22, 24, 35, 36, 38, 39, 41, 42, 51, 62, 103, 111, 112, 313, 314, 316:320]].reset_index(drop=True)
@@ -65,8 +80,22 @@ def get_fangraphs_data(x, y):
         # fill team id nulls with 0 to denote player is free agent (not rostered)
         stats['team_id'] = stats['team_id'].fillna(0).astype(int)
 
+        # save a reference to the original standard output
+        original_stdout = sys.stdout
+
+        with open('colkeys_fangraphs_pitcher.txt', 'w') as f:
+
+            # change the standard output to the file created
+            sys.stdout = f
+
+            # print column index and name to file
+            print(list(enumerate(stats.columns)))
+
+            # reset the standard output to its original output
+            sys.stdout = original_stdout
+
         # use certain metric columns only
-        df = stats.iloc[:, np.r_[0:6, 8, 9, 13, 16, 26, 40, 43:46, 47, 63, 111, 114, 323:332, 333, 334]].reset_index(drop=True)
+        df = stats.iloc[:, np.r_[0:6, 8, 9, 13, 15, 16, 26, 40, 43:46, 47, 63, 111, 114, 323:332, 333, 334]].reset_index(drop=True)
         
         # create new metric columns
         df['Barrels/BBE%'] = (df['Barrels']/df['BBE']).round(3)
